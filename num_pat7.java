8c78133090cbef551a7b9db03168378c481b3b45
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-07-01 08:58:42 +0530

    Add files via upload

---
+++ num_pat7.java
@@ -0,0 +1,13 @@
+class num_pat7
+{
+ public static void main(String args[])
+{
+  for(int i=5 ; i<=5 ;i++)
+ {
+   for( int j=5 ; j>=i ; j--)
+     System.out.print(j + " ");
+   
+     System.out.println();
+ }
+}
+}
