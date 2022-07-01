50f3dd14bbe33137f2249b12ac74bde57880488d
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-07-01 09:25:56 +0530

    Add files via upload

---
+++ num_pat8.java
@@ -0,0 +1,24 @@
+class num_pat8
+{
+ public static void main(String args[])
+{
+  for(int i=5 ; i<=5 ;i++)
+ {
+   for( int j=i ; j>=i ; j--)
+     System.out.print(j + " ");
+   
+     System.out.println();
+  }
+
+       for(int k=5 ; k<=5 ;k++)
+ {
+   for( int l=k ; l>=k ; l--)
+     System.out.print(l + " ");
+   
+     System.out.println();
+  }
+
+
+    
+ }
+}
