bf94df299deb51471d611c6802e7dee28274062a
Anjali Gunjal <107025907+anjalikb@users.noreply.github.com>
2022-07-02 09:59:29 +0530

    Add files via upload

---
+++ np32.java
@@ -0,0 +1,20 @@
+class np32
+{
+ public static void main(String args[])
+{
+for (int i = 1; i <= 5; i++)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int j = 1; j <= i; j++)
+            {
+                int c = a + b;
+                System.out.print(c + " ");
+                a = b;
+                b = c;
+            }
+            System.out.println();
+        }
+    }
+}
